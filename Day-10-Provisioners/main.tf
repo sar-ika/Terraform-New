91f0756c1d2831e36c99a57661e3a4f2dba6c907
sar-ika <168353160+sar-ika@users.noreply.github.com>
2024-05-29 18:23:15 +0530

    Update main.tf

--- Day-10-Provisioners/main.tf
+++ Day-10-Provisioners/main.tf
@@ -9,10 +9,9 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.0.0/26"
+  cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
 }
-
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.myvpc.id
 }
